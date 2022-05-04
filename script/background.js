a6db3df7f1a668134fcbd5e3ec0b6c62dfca4850
Alessandro <alessandrogreco@autistici.org>
2022-05-04 22:47:14 +0200

    Update
    - fix bugs

--- script/background.js
+++ script/background.js
@@ -1,52 +1,33 @@
-/*browser.contextMenus.create({
+
+browser.menus.create({
     id: "password-generator",
     title: "Generate Random Password!",
-    contexts: ["all"]
+    contexts: ["editable"]
 });
 
-browser.contextMenus.onClicked.addListener(function (info, tab) {
-    console.log("asd")
-    if (info.menuItemId == "password-generator") {
-        alert(1);
-        //elem = browser.contextMenus.bookmarkId;
-        //password = generateRandomStrongPassword();
-        //elem.value = password;
-        browser.contextMenus.bookmarkId = generateRandomStrongPassword();
+browser.menus.onClicked.addListener((info, tab) => {
+    if (info.editable) {
+        
+        payload = `browser.menus.getTargetElement(${info.targetElementId}).value = `
+        payload += "\"" + generateRandomStrongPassword() + "\";"
+        
+        browser.tabs.executeScript(tab.id, {
+            frameId: info.frameId,
+            code: payload
+        });
     }
-})*/
-
-// browser.menus.create({
-//     id: "password-generator",
-//     title: "Generate Random Password!",
-//     contexts: ["all"]
-//   }, onCreated);
-
-//   browser.menus.onClicked.addListener((info, tab) => {
-//     console.log("asd");
-//   });
-
-// browser.menus.create({
-//     id: "password-generator",
-//     title: "Generate Random Password 2!",
-//     contexts: ["all"],
-//     onclick(info, tab) {
-//         elem = `browser.menus.getTargetElement(${info.targetElementId})`;
-//         alert(elem)
-//         password = generateRandomStrongPassword();
-//         alert(password)
-//         elem.textContent = password;
-//     },
-//   });
+});
 
 // Source: https://stackoverflow.com/questions/9719570/generate-random-password-string-with-requirements-in-javascript
 function generateRandomStrongPassword() {
+
     var generatePassword = (
         length = 16,
         wishlist = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-_#$"[]{}.:,;<>£%&/\\|()=?^\''
     ) =>
         Array.from(crypto.getRandomValues(new Uint32Array(length)))
             .map((x) => wishlist[x % wishlist.length])
-            .join('')
-
+            .join('');
+    
     return generatePassword();
 }
